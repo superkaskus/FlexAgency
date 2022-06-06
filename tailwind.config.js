310a2be4a2a032c1250488497f51d205f21bb4b4
Rahim <twarikadams@gmail.com>
2022-06-06 10:29:47 +1000

    shuffled pages on the site

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const { indigo } = require("color-name");
 
 module.exports = {
-  content: ["./public/index.html", "./src/**/*.{html,js}"],
+  content: ["./**/*.html","./src/**/*.{html,js}"],
   theme: {
     fontFamily: {
       sans: ["Poppins", "sans-serif"],
