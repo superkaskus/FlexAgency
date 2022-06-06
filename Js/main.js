795d82f451246656496ceb7ebc2dbffd8ce23f90
Rahim <twarikadams@gmail.com>
2022-06-07 01:16:26 +1000

    Finalised build phew ..

--- Js/main.js
+++ Js/main.js
@@ -1,1 +1,43 @@
 
+ $( document ).ready(function() {
+
+
+    // Mobile menu toggle
+
+ $( "#menu" ).hide();
+
+$( "#hamburger , #close , .paa" ).click(function() { 
+        $("#menu").slideToggle();
+     
+});
+
+// BAsic Accordion sliders
+
+$(".slider").click(function() {
+    // var toogle =   $(".slider span > svg");
+   $(".sliderE ").slideToggle();
+$(".slider span > svg").toggleClass("rotate-180"); 
+});
+
+$(".slider-2").click(function() {
+    var toogle =   $(".slider-2 span > svg");
+   $(".sliderE-2 ").slideToggle();
+ toogle.toggleClass("rotate-180") ; 
+});
+
+
+$(".slider-3").click(function() {
+    var toogle =   $(".slider-3 span > svg");
+   $(".sliderE-3 ").slideToggle();
+ toogle.toggleClass("rotate-180") ; 
+});
+
+$(".slider-4").click(function() {
+    var toogle =   $(".slider-4 span > svg");
+   $(".sliderE-4").slideToggle();
+ toogle.toggleClass("rotate-180") ; 
+});
+
+})
+
+
